9305e61e52630169bf771c1c92519b3ee7466eff
Reginald T Carter iI <reginaldcarterii@gmail.com>
2018-06-17 23:32:59 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -7,11 +7,6 @@ def countdown(number)
   return "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep(number)
-  while number > 0
-    puts "#{number}" SECOND(S)!"
-    number -= 1
-    sleep 1
-  end
-  return "HAPPY NEW YEAR!"
+def countdown_with_sleep
+  sleep 5
 end
